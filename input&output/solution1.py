a9b9a9f91794c14b6ac41ddfeb9f2dcf49a98dd3
Hajar <hajaressalihi9@gmail.com>
2026-08-04 22:13:17 +0100

    Implement basic arithmetic operations in solution1.py

---
+++ input&output/solution1.py
@@ -0,0 +1,15 @@
+# input a float value for variable a here
+a=float(input("enter a variable: "))
+# input a float value for variable b here
+b=float(input("enter a variable: "))
+# output the result of addition here
+print(a," + ",b," = ",a+b)
+# output the result of subtraction here
+print(a," - ",b," = ",a-b)
+# output the result of multiplication here'
+print(a," * ",b," = ",a*b)
+# output the result of division here
+print(a," / ",b," = ",a/b)
+
+print("\nThat's all, folks!")
+
