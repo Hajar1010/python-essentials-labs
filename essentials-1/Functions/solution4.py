6e69a04dcda10c23219d67042f213ce78b8962e9
Hajar <hajaressalihi9@gmail.com>
2026-08-06 00:56:41 +0100

    Implement fuel efficiency conversion functions
    Added functions to convert between liters per 100 km and miles per gallon.

---
+++ essentials-1/Functions/solution4.py
@@ -0,0 +1,17 @@
+def liters_100km_to_miles_gallon(liters):
+    miles = 100000 / 1609.344
+    gallons = liters / 3.785411784
+    return miles / gallons
+
+def miles_gallon_to_liters_100km(miles):
+    liters = 3.785411784
+    hundred_km = (miles * 1609.344) / 100000
+    return liters / hundred_km
+
+print(liters_100km_to_miles_gallon(3.9))
+print(liters_100km_to_miles_gallon(7.5))
+print(liters_100km_to_miles_gallon(10.))
+print(miles_gallon_to_liters_100km(60.3))
+print(miles_gallon_to_liters_100km(31.4))
+print(miles_gallon_to_liters_100km(23.5))
+
