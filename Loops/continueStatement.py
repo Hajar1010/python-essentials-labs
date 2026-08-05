4bff167b3d4199a696f850ca3203051fd27ac164
Hajar <hajaressalihi9@gmail.com>
2026-08-05 01:48:35 +0100

    Add continue statement to skip vowels in input

---
+++ Loops/continueStatement.py
@@ -0,0 +1,18 @@
+word=str(input("enter a word"))
+word = word.upper()
+for letter in word:
+    if letter == "A" :
+      continue
+    elif letter == "E" :
+      continue
+    elif letter == "I" :
+      continue
+    elif letter == "O" :
+      continue
+    elif letter == "U" :
+      continue
+    else: 
+      print(letter)
+    
+    
+
