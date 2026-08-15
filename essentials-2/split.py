cc51cdff579721d2f08a3a9c2b1190f80fa93ba0
Hajar <hajaressalihi9@gmail.com>
2026-08-15 15:53:01 +0100

    Add mysplit function for string splitting
    Implement mysplit function to split strings by whitespace.

--- essentials-2/split.py
+++ essentials-2/split.py
@@ -1,1 +1,24 @@
-,,
+def mysplit(strng):
+    if string == ""
+        return []
+    words=[]
+    word=""
+    for i in string:
+          if i.isspace():
+              if word != "" :
+                  words.append(word)
+                  word = ""
+          else:
+              word+=i
+    if word != "":
+        words.append(word)
+
+    return words
+          
+
+print(mysplit("To be or not to be, that is the question"))
+print(mysplit("To be or not to be,that is the question"))
+print(mysplit("   "))
+print(mysplit(" abc "))
+print(mysplit(""))
+    
